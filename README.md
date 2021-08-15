5006df02e45f1db6a8e94e1b14334632df10135e
jaycammarano <jay.cammarano@gmail.com>
2021-08-14 20:02:53 -0400

    Changed title to generic css to dark mode

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 Boilerplate for working with electron, typescript, and vue with Tailwind as a css framework.
 
 To get started:
+
 After cloning the repo run `npm install`
+
 Then run `npm run electron:serve` to see the app.
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to the TypeScript Music Player" />
+    <img class="m-auto" alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Vue Electron App" />
   </div>
 </template>
 
